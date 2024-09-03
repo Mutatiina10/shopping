5a3950e3c6c8406bcc275c60b870723425ebc05b
Mutatiina10 <mutatiinane@gmail.com>
2024-09-03 08:58:48 +0300

    save

--- business/onlineapp/urls.py
+++ business/onlineapp/urls.py
@@ -3,8 +3,9 @@ from .import views
 
 
 
-
+#these are the app URLs 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('',views.products, name='product_list'),
 ]
--- business/onlineapp/views.py
+++ business/onlineapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
+'''''
 def index(request):
     return render(request, 'onlineapp/index.html')
+    '''''
+def products(request):
+    return render(request, 'onlineapp/product_list.html')
